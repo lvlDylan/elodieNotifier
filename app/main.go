2c5c29e587840bd62b7a23fcbf02ce7e69a2421e
Dylan <55927823+lvlDylan@users.noreply.github.com>
2025-12-07 15:24:04 +0100

    Update main.go
    Updating imgPath var.
    Removing testing one.

--- app/main.go
+++ app/main.go
@@ -18,7 +18,7 @@ const wsURL = "wss://elodie.dylanlv.dev/ws/"
 
 func notifySend(title string, message string) {
 	appName := "Bibus Notifications"
-	imagePath := "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"
+	imagePath := "example/path/img.jpg"
 	cmd := exec.Command("notify-send", "-a", appName, "-i", imagePath, title, message)
 
 	if err := cmd.Run(); err != nil {
